a9afc4c9bf4203331d93d5a0aebf4a89cf0bf09a
JGil <jgil@papamono.es>
2021-01-12 20:56:16 +0100

    fix: landing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" style="text-align: center;">
+    <a href="https://lapuertamarilla.es" rel="noopener noreferrer">
+      <img alt="Vue logo" src="https://lapuertamarilla.es/images/logo.png">
+    </a>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  // name: 'Home',
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
