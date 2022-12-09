d475bb8dc5fc24a98292e65ed7fe65cecbcdfcc4
JGil <jgil@papamono.es>
2022-12-09 11:38:01 +0100

    [router] fix: router fix try

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Router from 'vue-router';
+
 import Home from '../views/Home.vue';
 import PageRestaurant from '@/pages/PageRestaurant.vue';
 import PageOrder from '@/pages/PageOrder.vue';
@@ -14,92 +15,93 @@ import PageDeleteAccount from '@/pages/PageDeleteAccount';
 import PageRegister from '@/pages/PageRegister';
 import PageLogin from '@/pages/PageLogin';
 
-Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/register/',
-    name: 'PageRegister',
-    component: PageRegister,
-  },
-  {
-    path: '/login/',
-    name: 'PageLogin',
-    component: PageLogin,
-  },
-  {
-    path: '/restaurant/:id',
-    name: 'PageRestaurant',
-    component: PageRestaurant,
-  },
-  {
-    path: '/order/',
-    name: 'PageOrder',
-    component: PageOrder,
-    meta: { onlyAuthUser: true },
-  },
-  {
-    path: '/success/:orderId',
-    name: 'PageSuccess',
-    component: PageSuccess,
-    meta: { onlyAuthUser: true },
-  },
-  {
-    path: '/PageSuccessActivate/',
-    name: 'PageSuccessActivate',
-    component: PageSuccessActivate,
-  },
-  {
-    path: '/PageSuccessReset/',
-    name: 'PageSuccessReset',
-    component: PageSuccessReset,
-  },
-  {
-    path: '/PageSuccessDelete/',
-    name: 'PageSuccessDelete',
-    component: PageSuccessDelete,
-  },
-  {
-    path: '/backoffice/:id',
-    name: 'PedidosPendientes',
-    component: PagePedidosPendientes,
-  },
-  {
-    path: '/users/:hash/activate',
-    name: 'PageActivateUser',
-    component: PageActivateUser,
-    meta: { onlyGuestUser: true },
-  },
-  {
-    path: '/users/:hash/reset',
-    name: 'PageResetPassword',
-    component: PageResetPassword,
-    meta: { onlyGuestUser: true },
-  },
-  {
-    path: '/users/:hash/delete',
-    name: 'PageDeleteAccount',
-    component: PageDeleteAccount,
-    meta: { onlyGuestUser: true },
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-];
-
-const router = new VueRouter({
-  routes,
+Vue.use(Router);
+export default new Router({
+  mode: 'history',
+  // watch: {
+  //   $route(to, from) {
+  //     // react to route changes...
+  //   },
+  // },
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/register/',
+      name: 'PageRegister',
+      component: PageRegister,
+    },
+    {
+      path: '/login/',
+      name: 'PageLogin',
+      component: PageLogin,
+    },
+    {
+      path: '/restaurant/:id',
+      name: 'PageRestaurant',
+      component: PageRestaurant,
+    },
+    {
+      path: '/order/',
+      name: 'PageOrder',
+      component: PageOrder,
+      meta: { onlyAuthUser: true },
+    },
+    {
+      path: '/success/:orderId',
+      name: 'PageSuccess',
+      component: PageSuccess,
+      meta: { onlyAuthUser: true },
+    },
+    {
+      path: '/PageSuccessActivate/',
+      name: 'PageSuccessActivate',
+      component: PageSuccessActivate,
+    },
+    {
+      path: '/PageSuccessReset/',
+      name: 'PageSuccessReset',
+      component: PageSuccessReset,
+    },
+    {
+      path: '/PageSuccessDelete/',
+      name: 'PageSuccessDelete',
+      component: PageSuccessDelete,
+    },
+    {
+      path: '/backoffice/:id',
+      name: 'PedidosPendientes',
+      component: PagePedidosPendientes,
+    },
+    {
+      path: '/users/:hash/activate',
+      name: 'PageActivateUser',
+      component: PageActivateUser,
+      // meta: { onlyGuestUser: true },
+    },
+    {
+      path: '/users/:hash/reset',
+      name: 'PageResetPassword',
+      component: PageResetPassword,
+      // meta: { onlyGuestUser: true },
+    },
+    {
+      path: '/users/:hash/delete',
+      name: 'PageDeleteAccount',
+      component: PageDeleteAccount,
+      // meta: { onlyGuestUser: true },
+    },
+    {
+      path: '/about',
+      name: 'About',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    },
+  ],
 });
-
-export default router;
