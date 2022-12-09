1ae16eabef5cbf1696f9f5b11cb03a670af47be8
JGil <jgil@papamono.es>
2022-12-09 11:53:19 +0100

    [router] fix: router try to fix

--- src/router/index.js
+++ src/router/index.js
@@ -94,14 +94,14 @@ export default new Router({
       component: PageDeleteAccount,
       // meta: { onlyGuestUser: true },
     },
-    {
-      path: '/about',
-      name: 'About',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ '../views/About.vue'),
-    },
+    // {
+    //   path: '/about',
+    //   name: 'About',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    // },
   ],
 });
